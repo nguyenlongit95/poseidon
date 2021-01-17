51db8f072282ce4c1e65c464c9add555e1d22877
unknown <nguyenlongit95@gmail.com>
2021-01-17 21:05:46 +0700

    update config database

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'id15937639_poseidon' );
+define( 'DB_NAME', 'poseidon' );
 
 /** MySQL database username */
-define( 'DB_USER', 'id15937639_root' );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '>5+Zs[[#i#lKhzuF' );
+define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
